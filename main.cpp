fb6b4fe2d11271c43d5082fcf295b6fcea8a937d
mazak <michal.maziarz12@gmail.com>
2023-05-07 23:40:34 +0200

    END

--- main.cpp
+++ main.cpp
@@ -10,24 +10,27 @@ void mstClient();
 
 void shortestPathClient();
 
+void performTests();
+
 int main() {
-    TestPerformer::allTests();
-    //    using namespace std;
-//    bool program = true;
-//    string option;
-//    while (program) {
-//        printMenu();
-//        getline(cin, option);
-//        if (option == "1") {
-//            mstClient();
-//        } else if (option == "2") {
-//            shortestPathClient();
-//        } else if (option == "3") {
-//            program = false;
-//        } else {
-//            cout << "Nie ma takiej opcji." << endl;
-//        }
-//    }
+    using namespace std;
+    bool program = true;
+    string option;
+    while (program) {
+        printMenu();
+        getline(cin, option);
+        if (option == "1") {
+            mstClient();
+        } else if (option == "2") {
+            shortestPathClient();
+        } else if (option == "3") {
+          performTests();
+        } else if (option == "4") {
+            program = false;
+        } else {
+            cout << "Nie ma takiej opcji." << endl;
+        }
+    }
 
 }
 
@@ -36,7 +39,8 @@ void printMenu() {
     cout << "Projekt ze SDIZO #2" << endl;
     cout << "1) Minimalne drzewa rozpinające" << endl;
     cout << "2) Najkrotsza sciezka" << endl;
-    cout << "3) wyjdz" << endl;
+    cout << "3) Testy" << endl;
+    cout << "4) wyjdz" << endl;
     cout << "Wybierz opcje: ";
 }
 
@@ -50,4 +54,8 @@ void shortestPathClient() {
     auto *client = new ShortestPathClient();
     client->start();
     delete client;
+}
+
+void performTests() {
+    TestPerformer::allTests();
 }
